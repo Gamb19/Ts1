141d59bcf5bf7c00ed6a8136bae42df0fec583f0
gabriel molina <molinaborges02@gmail.com>
2023-04-18 14:53:22 -0500

    archivos

---
+++ app.ts
@@ -0,0 +1,13 @@
+function addTask() {
+    let input = document.getElementById("input-text");
+    let div = document.getElementById("div-task");
+    let btnAdd= document.getElementById("btn-add");
+    console.log("Si agarro");
+    btnAdd?.addEventListener("click", ()=>{
+        console.log("Si sirve");
+        
+    });
+    
+}
+console.log("Hola");
+
